632efdcd8dbc053084e16ddd6005ba93f8e376a9
Muhammadhanif0901 <167406613+Muhammadhanif0901@users.noreply.github.com>
2024-05-12 19:26:02 +0700

    Update listbukutersedia.c

--- listbukutersedia.c
+++ listbukutersedia.c
@@ -1,4 +1,5 @@
 #include "header.h"
+#include <stdio.h>
 
 void listBukuTersedia() {
     printf("List Buku Tersedia:\n");
@@ -9,10 +10,14 @@ void listBukuTersedia() {
         printf("Gagal membuka file.\n");
         return;
     }
+    printf("\n\t\t\t=======================================================================================");
+    printf("\n\t\t\t\t\t\t       LIHAT BUKU TERSEDIA       \n");
+    printf("\n\t\t\t=======================================================================================\n");
+    printf("Daftar Buku yang tersedia:\n");
 
     // Membaca dan menampilkan data buku dari file
     Buku buku;
-    while (fscanf(file, "%u,%[^,],%[^,],%[^,],%u,%u,%u\n", &buku.id, buku.judul, buku.penulis, buku.penerbit, &buku.halaman, &buku.tahun, &buku.jumlah_tersedia) != EOF) {
+    while (fscanf(file, "%u,%[^,],%[^,],%[^,],%u,%u,%u\n", &buku.id, buku.judul, buku.penulis, buku.penerbit, &buku.halaman, &buku.tahun, &buku.jumlah_tersedia) == 7) {
         if (buku.jumlah_tersedia > 0) {
             printf("ID: %u\n", buku.id);
             printf("Judul: %s\n", buku.judul);
