efd69bdb7e67d1aa61935ff467d10334615bb617
YulliErisna <167390598+YulliErisna@users.noreply.github.com>
2024-05-14 20:42:43 +0700

    tambahbuku.c

--- tambahbuku.c
+++ tambahbuku.c
@@ -25,7 +25,7 @@ void tambahBuku() {
     scanf("%u", &buku.jumlah_tersedia);
 
     // Menyimpan data buku ke dalam file
-    FILE *file = fopen("berekstensi.txt", "a");
+    FILE *file = fopen("berekstensi.txt", "a\n");
     if (file == NULL) {
         printf("Gagal membuka file.\n");
         return;
