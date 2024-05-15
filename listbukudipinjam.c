7a55e0893090959c63addc65f5ce0330eb00b8c0
Dwifitriawindy <167391058+Dwifitriawindy@users.noreply.github.com>
2024-05-16 02:41:16 +0700

    Update listbukudipinjam.c

--- listbukudipinjam.c
+++ listbukudipinjam.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "header.h"
 
 void listBukuDipinjam() {
-    // Membuka file database
     FILE *file = fopen("bukudipinjam.txt", "r");
     if (file == NULL) {
         printf("Gagal membuka file.\n");
         return;
     }
+
     printf("\n\t\t\t=======================================================================================");
     printf("\n\t\t\t\t\t\t       DAFTAR BUKU DIPINJAM       \n");
     printf("\n\t\t\t=======================================================================================\n");
     printf("Daftar Buku yang anda pinjam:\n");
 
-    // Membaca dan menampilkan data buku dari file
+
     Buku buku;
     while (fscanf(file, "%u,%[^,],%[^,],%[^,],%u,%u,%u\n", &buku.id, buku.judul, buku.penulis, buku.penerbit, &buku.halaman, &buku.tahun, &buku.jumlah_tersedia) == 7) {
-        if (buku.jumlah_tersedia > 0) {
-            printf("ID: %u\n", buku.id);
-            printf("Judul: %s\n", buku.judul);
-            printf("Penulis: %s\n", buku.penulis);
-            printf("Penerbit: %s\n", buku.penerbit);
-            printf("Jumlah Halaman: %u\n", buku.halaman);
-            printf("Tahun Terbit: %u\n", buku.tahun);
-            printf("\n");
-        }
+        printf("ID: %u\n", buku.id);
+        printf("Judul: %s\n", buku.judul);
+        printf("Penulis: %s\n", buku.penulis);
+        printf("Penerbit: %s\n", buku.penerbit);
+        printf("Halaman: %u\n", buku.halaman);
+        printf("Tahun: %u\n", buku.tahun);
+        printf("Jumlah Tersedia: %u\n", buku.jumlah_tersedia);
+        printf("\n");
     }
 
     fclose(file);
